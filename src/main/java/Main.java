a9422869b994f5300660944e54f4ec69d2ec9384
Arhat161 <Arhat161@users.noreply.github.com>
2022-05-18 22:22:56 +0300

    beta version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,21 @@
 import java.io.File;
+import java.net.ServerSocket;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        // Uncomment bottom strings for testing Search Engine
+
+        /*BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+        System.out.println(engine.search("бизнес"));
+        System.out.println(engine.search("DevOps"));*/
+
+        // Here create server, who's response to requests
+        // Server will listen on port 8989
+        // and answer /{word} -> returning value of search(word) in JSON-format
+
+        SearchServer searchServer = new SearchServer(8989);
+        searchServer.start();
     }
 }
